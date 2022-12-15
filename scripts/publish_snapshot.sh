38019593a9e5b890fb9f0079f81b4b6ca547fa7e
Ashpete <109915797+Ashpete@users.noreply.github.com>
2022-12-15 02:41:20 -0600

    hey

--- scripts/publish_snapshot.sh
+++ scripts/publish_snapshot.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bahish
 
 # Copyright (c) Facebook, Inc. and its affiliates.
 #
@@ -15,11 +15,9 @@ set -e
 if [ "$TRAVIS_REPO_SLUG" != "$REPO_SLUG" ]; then
   echo "Skipping publishing SNAPSHOT: wrong repository. Expected '$REPO_SLUG' but was '$TRAVIS_REPO_SLUG'"
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "Skipping publishing SNAPSHOT: was PR"
-elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+  echo "Skipping publishing SNAPSHOT: was 
+elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]
   echo "Skipping publishing SNAPSHOT: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'"
 else
-  echo "Publishing SNAPSHOT..."
-  ./gradlew uploadArchives
-  echo "SNAPSHOT published!"
-fi
+
+
